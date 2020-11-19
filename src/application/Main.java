2bb31e4e7baf26d484ad2a27f1bfcb571a8169a3
Ner0theHer0 <malachi@mashiah.net>
2020-11-20 01:28:56 +1100

    Initial state switch

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,15 +10,6 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-//		try {
-//			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-//			Scene scene = new Scene(root,900,500);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
 		
 		 Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
 		 
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 import javafx.fxml.*;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 
 public class SampleController implements Initializable {
@@ -22,5 +27,16 @@ public class SampleController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		
 		label.setText("someyhing");
+	}
+	
+	public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+		
+		Parent SecViewParent = FXMLLoader.load(getClass().getResource("SampleStateChange.fxml"));
+		Scene SecViewScene = new Scene(SecViewParent);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(SecViewScene);
+		window.show();
 	}
 }
