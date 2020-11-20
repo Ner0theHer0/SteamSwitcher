650fdbb13db7e62aa73e9f88782d4f2dacadebe9
Ner0theHer0 <malachi.mashiah@students.mq.edu.au>
2020-11-20 14:57:38 +1100

    Basic material design and 4 pane switch

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,14 +12,19 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		 Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+		 Parent root = FXMLLoader.load(getClass().getResource("PaneOne.fxml"));
 		 
-	        
 	     Scene scene = new Scene(root);
 	     scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	        
 	     primaryStage.setScene(scene);
 	     primaryStage.show();
+	     
+	     SampleController ctr = new SampleController();
+	     
+	     FXMLLoader ld = new FXMLLoader();
+	     ld.setController(ctr);
+	     
 //		
 	}
 	
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -17,10 +18,20 @@ public class SampleController implements Initializable {
 	@FXML
 	private Label label;
 	
+	public Button button;
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	
+	
+	
 
 	public void buttonPushed() {
+		
+		
 		String m = "mess";
 		this.label.setText(m);
+		//this.button.setStyle("-fx-background-color: #152238;");
 	}
 
 	@Override
@@ -29,14 +40,52 @@ public class SampleController implements Initializable {
 		label.setText("someyhing");
 	}
 	
-	public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+	public void changeScreenButtonPushed1(ActionEvent event) throws IOException {
 		
-		Parent SecViewParent = FXMLLoader.load(getClass().getResource("SampleStateChange.fxml"));
-		Scene SecViewScene = new Scene(SecViewParent);
+		Parent FirViewParent = FXMLLoader.load(getClass().getResource("PaneOne.fxml"));
+		Scene FirViewScene = new Scene(FirViewParent);
 		
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 		
+		
+		window.setScene(FirViewScene);
+		window.show();
+	}
+	
+	public void changeScreenButtonPushed2(ActionEvent event) throws IOException {
+		
+		Parent SecViewParent = FXMLLoader.load(getClass().getResource("PaneTwo.fxml"));
+		Scene SecViewScene = new Scene(SecViewParent);
+		//SecViewScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		
 		window.setScene(SecViewScene);
 		window.show();
+	}
+	
+	public void changeScreenButtonPushed3(ActionEvent event) throws IOException {
+		
+		Parent ThirViewParent = FXMLLoader.load(getClass().getResource("PaneThree.fxml"));
+		Scene ThirViewScene = new Scene(ThirViewParent);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		
+		window.setScene(ThirViewScene);
+		window.show();
+	}
+
+	public void changeScreenButtonPushed4(ActionEvent event) throws IOException {
+	
+	Parent ForViewParent = FXMLLoader.load(getClass().getResource("PaneFour.fxml"));
+	Scene ForViewScene = new Scene(ForViewParent);
+	
+	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+	
+	
+	window.setScene(ForViewScene);
+	window.show();
 	}
 }
