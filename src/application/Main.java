59de810b8f84924a0623eb74eece1b6543d6fb94
Ner0theHer0 <malachi.mashiah@students.mq.edu.au>
2020-11-20 18:01:35 +1100

    Added basic popup (messy)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +14,6 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		 Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
-		 
 	        
 	     Scene scene = new Scene(root);
 	     scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -4,18 +4,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 import javafx.fxml.*;
+import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 
 public class SampleController implements Initializable {
 	
 	@FXML
+	private Button closeButton;
 	private Label label;
+	public Button addButton;
 	
 
 	public void buttonPushed() {
@@ -26,7 +33,21 @@ public class SampleController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		
-		label.setText("someyhing");
+	}
+	
+	public void addAccountButtonPushed(ActionEvent event) throws IOException {
+		Parent popParent = FXMLLoader.load(getClass().getResource("Popup.fxml"));
+		Stage popup = new Stage();
+		
+		popup.initModality(Modality.APPLICATION_MODAL);
+		popup.setTitle("Add Account");
+		popup.setMinWidth(300);
+		
+		
+		Scene scene = new Scene(popParent);
+		popup.setScene(scene);
+		
+		popup.showAndWait();
 	}
 	
 	public void changeScreenButtonPushed(ActionEvent event) throws IOException {
@@ -38,5 +59,10 @@ public class SampleController implements Initializable {
 		
 		window.setScene(SecViewScene);
 		window.show();
+	}
+	
+	@FXML 
+	public void handleCloseButtonAction(ActionEvent event) {
+		((Stage)(((Node)event.getSource()).getScene().getWindow())).close();
 	}
 }
