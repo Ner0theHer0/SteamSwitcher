9f87cbf4936633c756366edfe54c5a318c007989
Ner0theHer0 <malachi@mashiah.net>
2020-11-23 01:22:32 +1100

    Integrated adding user to client

---
+++ src/application/Backend.java
@@ -0,0 +1,107 @@
+package application;
+
+
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Backend {
+	
+	private int numUsers;
+	HashMap<String, String> map = new HashMap<String, String>();
+	private String path = "src/application/users.txt";
+	
+	public Backend() {
+		
+		readFromFile();
+		
+		System.out.println(numUsers);
+		
+		for (HashMap.Entry<String, String> entry : map.entrySet()) {
+		    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		
+	}
+	
+	public boolean addUser(String user, String pass) {
+		
+		if (map.containsKey(user)) {
+			System.out.println("User already exists!");
+			return false;
+		} 
+		
+		else {
+			user.replaceAll("\\s+","");
+			
+			try {
+				
+				BufferedWriter out = new BufferedWriter(new FileWriter(path, true)); 
+		        out.write(user + " " + pass + "\n"); 
+		        out.close();
+		        
+			    System.out.println("Successfully created new user.");
+			    
+			} catch (Exception e) {
+				System.out.println("Couldn't create new user: " + e);
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+	
+	public void removeUser(String user) {
+		
+		if (!map.containsKey(user)) {
+			System.out.println("This user does not exist");
+		}
+		
+		else {
+			
+			
+			map.remove(user);
+			
+			try {
+				FileWriter writer = new FileWriter(path);
+				
+			    writer.write("");
+			    writer.close();
+			    
+			    for (String key : map.keySet()) {
+			    	BufferedWriter out = new BufferedWriter(new FileWriter(path, true)); 
+			        out.write(key + " " + map.get(key) + "\n"); 
+			        out.close();
+				}
+			    
+			    
+			} catch (Exception e) {
+				System.out.println("Something went wrong");
+			}
+		}
+	}
+	
+	public void readFromFile() {
+		try {
+			Scanner in = new Scanner(new FileReader(path));
+			numUsers = 0;
+			
+			
+			while (in.hasNext()) {
+				
+				String[] thisLine = in.nextLine().split("\\s+");
+				map.put(thisLine[0], thisLine[1]);
+				numUsers++;
+					
+			}
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("something went wrong: " + e);
+		}
+	}
+	
+}
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 import javafx.fxml.*;
-import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -21,22 +20,39 @@ public class SampleController implements Initializable {
 	
 	@FXML
 	private Button closeButton;
-	private Label label;
+	
+	@FXML
+	public Label label1;
+	@FXML 
 	public Button addButton;
+	
+	public Backend bk;
+	
+	@FXML
+	private TextField usrn;
+	
+	@FXML
+	private TextField passw;
+	
+	@FXML
+	private Button confirm;
+
 	
 
 	public void buttonPushed() {
-		String m = "mess";
-		this.label.setText(m);
+		//String m = "mess";
+		this.label1.setText("asdasdasdasdad");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+		bk = new Backend();
 	}
+	
 	
 	public void addAccountButtonPushed(ActionEvent event) throws IOException {
 		Parent popParent = FXMLLoader.load(getClass().getResource("Popup.fxml"));
+		
 		Stage popup = new Stage();
 		
 		popup.initModality(Modality.APPLICATION_MODAL);
@@ -46,8 +62,57 @@ public class SampleController implements Initializable {
 		
 		Scene scene = new Scene(popParent);
 		popup.setScene(scene);
+		popup.showAndWait();
+		
+	}
+	
+	@FXML
+	public void addAcc(ActionEvent event) throws IOException {
+		
+		//bk = new Backend();
+		
+		boolean sw = false;
+		
+		if (usrn.getText() != null && passw.getText() != null) {
+			
+			if (bk.addUser(usrn.getText(), passw.getText())) {
+				sw = true;
+			} else {
+				sw = false;
+			}
+			
+		}
+		
+		FXMLLoader ld = new FXMLLoader();
+		ld.setLocation(getClass().getResource("Confirmation.fxml"));
+		
+		Parent popParent = ld.load();
+		
+		Stage popup = new Stage();
+		
+		popup.initModality(Modality.APPLICATION_MODAL);
+		popup.setTitle("Add Account");
+		popup.setMinWidth(300);
+		
+		
+		Scene scene = new Scene(popParent);
+		
+		
+		popup.setScene(scene);
+		ConfirmController ctr = ld.getController();
+		
+		if (sw == true) {
+			
+			ctr.setLabel("User added successfully");
+		} else {
+			ctr.setLabel("Error adding user");
+		}
 		
 		popup.showAndWait();
+		
+		((Stage)(((Node)event.getSource()).getScene().getWindow())).close();
+		
+		bk = new Backend();
 	}
 	
 	public void changeScreenButtonPushed(ActionEvent event) throws IOException {
@@ -63,6 +128,7 @@ public class SampleController implements Initializable {
 	
 	@FXML 
 	public void handleCloseButtonAction(ActionEvent event) {
+		
 		((Stage)(((Node)event.getSource()).getScene().getWindow())).close();
 	}
 }
